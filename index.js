cf9b14ba1a39b3e5cadac5dd3e560e82a6c68efd
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-27 20:03:40 +0200

    exo1 finished

---
+++ index.js
@@ -0,0 +1,8 @@
+// Exo1
+
+// let reversedString=""
+// const sortLetters = (a) => {
+//     return reversedString = a.split("").reverse().join("");
+// }
+// sortLetters("string")
+// console.log(reversedString);

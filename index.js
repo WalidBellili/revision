138ff083b39bf73f72cbb133afd4d1c0f7014247
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-27 21:41:11 +0200

    exo3 finish

--- index.js
+++ index.js
@@ -6,3 +6,39 @@
 // }
 // sortLetters("string")
 // console.log(reversedString);
+// let stringX = 0
+// let stringY = 0
+// const countEach = (xoxo) => {
+//     for (let i = 0; i <= xoxo.length; i++ ) {
+        
+//         if (xoxo[i]=== "x") {
+//             stringX += 1
+           
+//         } else {
+//             stringY += 1            
+//         }  
+
+//     }
+//     // console.log(stringX);
+//     // console.log(stringY);
+//     if (stringX === stringY) {        
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// } 
+// countEach("xoxoxoxoxoxoxoxxx")
+
+// exo3
+let palindrome = ""
+const isPalindrome = (string) => {
+    palindrome = string.split("").reverse().join("")
+    console.log(palindrome);
+    if (string === palindrome) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+
+}
+isPalindrome("Palindrome !")
